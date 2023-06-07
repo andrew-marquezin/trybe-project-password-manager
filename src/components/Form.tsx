e0da46c35ee982bc5fb2c9c84f86a89afe0e4383
andrew-marquezin <andrewmmarinho@gmail.com>
2023-06-06 21:39:06 -0300

    requisito 2 foi

---
+++ src/components/Form.tsx
@@ -0,0 +1,18 @@
+function Form() {
+  return (
+    <div>
+      <label htmlFor="service-name-inpt">Nome do serviço</label>
+      <input type="text" name="nome-do-serviço" id="service-name-inpt" />
+      <label htmlFor="login-inpt">Login</label>
+      <input type="text" name="login" id="login-inpt" />
+      <label htmlFor="password-inpt">Senha</label>
+      <input type="password" name="senha" id="password-inpt" />
+      <label htmlFor="url-inpt">URL</label>
+      <input type="text" name="url" id="url-inpt" />
+      <button type="submit">Cadastrar</button>
+      <button>Cancelar</button>
+    </div>
+  );
+}
+
+export default Form;
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h1>Gerenciador de Senhas</h1>
+  );
+}
+
+export default Header;

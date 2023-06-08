601af19213700649cc67d6fce59e66b7f7af7fcf
andrew-marquezin <andrewmmarinho@gmail.com>
2023-06-08 14:42:25 -0300

    requisito 3 foi

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './components/Header';
 import Form from './components/Form';
+import Header from './components/Header';
 
 function App() {
   return (
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+
 function Form() {
-  return (
+  const [showForm, setShowForm] = useState(false);
+
+  function handleCancel() {
+    setShowForm(!showForm);
+  }
+
+  const newBtn = (
     <div>
+      <button onClick={ handleCancel }>Cadastrar nova senha</button>
+    </div>
+  );
+
+  const form = (
+    <form>
       <label htmlFor="service-name-inpt">Nome do serviço</label>
       <input type="text" name="nome-do-serviço" id="service-name-inpt" />
       <label htmlFor="login-inpt">Login</label>
@@ -10,7 +24,13 @@ function Form() {
       <label htmlFor="url-inpt">URL</label>
       <input type="text" name="url" id="url-inpt" />
       <button type="submit">Cadastrar</button>
-      <button>Cancelar</button>
+      <button onClick={ handleCancel }>Cancelar</button>
+    </form>
+  );
+
+  return (
+    <div>
+      {showForm ? form : newBtn}
     </div>
   );
 }

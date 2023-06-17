f209dd086107c7cd409391c78b7ad01724bfa28d
andrew-marquezin <andrewmmarinho@gmail.com>
2023-06-16 22:40:14 -0300

    por que não funciona?

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -85,6 +85,18 @@ function Form() {
           onChange={ handleChange }
         />
       </label>
+      {formInfo.password.trim().length > 8
+        ? <p className="valid-password-check">Possuir 8 ou mais caracteres</p>
+        : <p className="invalid-password-check">Possuir 8 ou mais caracteres</p>}
+      {formInfo.password.trim().length < 16
+        ? <p className="valid-password-check">Possuir até 16 caracteres</p>
+        : <p className="invalid-password-check">Possuir até 16 caracteres</p>}
+      {/[a-zA-Z\d]/.test(formInfo.password) && formInfo.password.trim().length > 0
+        ? <p className="valid-password-check">Possuir letras e números</p>
+        : <p className="invalid-password-check">Possuir letras e números</p>}
+      {/[!@#$%^&*/\\.,><-]/.test(formInfo.password)
+        ? <p className="valid-password-check">Possuir algum caractere especial</p>
+        : <p className="invalid-password-check">Possuir algum caractere especial</p>}
       <label>
         URL
         <input

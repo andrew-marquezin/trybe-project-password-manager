4c0b6f8c0fe204da95652444171faa9e1a2461d6
andrew-marquezin <andrewmmarinho@gmail.com>
2023-06-18 16:06:28 -0300

    requisito 5 funciona agora

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -93,10 +93,10 @@ function Form() {
       {formInfo.password.trim().length < 16
         ? <p className="valid-password-check">Possuir até 16 caracteres</p>
         : <p className="invalid-password-check">Possuir até 16 caracteres</p>}
-      {/[a-zA-Z\d]/.test(formInfo.password) && formInfo.password.trim().length > 0
+      {/[a-zA-Z]/.test(formInfo.password) && /\d/.test(formInfo.password)
         ? <p className="valid-password-check">Possuir letras e números</p>
         : <p className="invalid-password-check">Possuir letras e números</p>}
-      {/[!@#$%^&*/\\.,><-]/.test(formInfo.password)
+      {/\W/.test(formInfo.password)
         ? <p className="valid-password-check">Possuir algum caractere especial</p>
         : <p className="invalid-password-check">Possuir algum caractere especial</p>}
       <label>
